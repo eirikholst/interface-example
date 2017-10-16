fc239d158464cbefc1dde13f553fd336e463c11e
Eirik Holst <eirik.holst@bouvet.no>
2017-10-16 18:32:31 +0200

    Changed Abs() to Length()

--- main.go
+++ main.go
@@ -8,17 +8,17 @@ import (
 func main() {
 	v2 := geometry.Vector2D{X: 3, Y: 4}
 	printComponents(v2)
-	printAbs(v2)
+	printLength(v2)
 
 	v3 := geometry.Vector3D{X: 3, Y: 4, Z: 5}
 	printComponents(v3)
-	printAbs(v3)
+	printLength(v3)
 }
 
 func printComponents(vector geometry.Vector) {
 	fmt.Println(vector.GetComponents())
 }
 
-func printAbs(vector geometry.Vector) {
+func printLength(vector geometry.Vector) {
 	fmt.Println(vector.Length())
 }
