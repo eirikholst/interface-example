a8dd772c665da7374e7f4e937a9bfd4e7c3dc4f8
Eirik Holst <eirik.holst@bouvet.no>
2017-10-17 17:54:26 +0200

    simplified

--- main.go
+++ main.go
@@ -7,16 +7,10 @@ import (
 
 func main() {
 	v2 := geometry.Vector2D{X: 3, Y: 4}
-	printComponents(v2)
 	printLength(v2)
 
 	v3 := geometry.Vector3D{X: 3, Y: 4, Z: 5}
-	printComponents(v3)
 	printLength(v3)
-}
-
-func printComponents(vector geometry.Vector) {
-	fmt.Println(vector.GetComponents())
 }
 
 func printLength(vector geometry.Vector) {
--- pkg/geometry/vector.go
+++ pkg/geometry/vector.go
@@ -4,7 +4,6 @@ import "math"
 
 type Vector interface {
 	Length() float64
-	GetComponents() []float64
 }
 
 type Vector2D struct {
@@ -15,18 +14,10 @@ func (v Vector2D) Length() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
-func (v Vector2D) GetComponents() []float64 {
-	return []float64{v.X, v.Y}
-}
-
 type Vector3D struct {
 	X, Y, Z float64
 }
 
 func (v Vector3D) Length() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y + v.Z*v.Z)
-}
-
-func (v Vector3D) GetComponents() []float64 {
-	return []float64{v.X, v.Y, v.Z}
 }
