f7a28c34babd0ea2c85b324c776cfdd7336b40eb
Eirik Holst <eirik.holst@bouvet.no>
2018-04-12 09:37:07 +0200

    simplified

--- pkg/geometry/vector.go
+++ pkg/geometry/vector.go
@@ -10,13 +10,14 @@ type Vector2D struct {
 	X, Y float64
 }
 
+type Vector3D struct {
+	X, Y, Z float64
+}
+
 func (v Vector2D) Length() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
-type Vector3D struct {
-	X, Y, Z float64
-}
 
 func (v Vector3D) Length() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y + v.Z*v.Z)
